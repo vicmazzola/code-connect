73774dd19a1775c67183bb62ae6b59b2e3ee2015
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-24 17:53:17 -0300

    add readFile js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const inputUpload = document.getElementById("image-upload")
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+function readFileContent(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            resolve({url: reader.result, nome: file.name})
+        }
+
+        reader.onerror = () => {
+            reject(`Error reading the file ${file.name}`)
+        }
+
+        reader.readAsDataURL(file)
+    })
+}
+
